7dc759769c230fc921e375cd0e45ece0f4d610be
syukrianuar <2018638988@student.uitm.edu.my>
2021-10-22 16:26:25 +0800

    second commit

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({ Key? key }) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({ Key? key }) : super(key: key);
 
   @override
-  _LoginState createState() => _LoginState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginPageState extends State<LoginPage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  GlobalKey<FormState>globalFormKey = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Theme.of(context).accentColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  margin: EdgeInsets.symmetric(vertical: 85, horizontal: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Theme.of(context).primaryColor,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).hintColor.withOpacity(0.2),
+                        offset: Offset(0,10),
+                        blurRadius: 20),
+                    ],
+                  ),
+                  child: Form(
+                    key: globalFormKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 25,),
+                        Text(
+                          'Login', 
+                          style: Theme.of(context).textTheme.headline2
+                        ),
+                        SizedBox(height: 20,),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
       
     );
   }
